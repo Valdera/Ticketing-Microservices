cb21fa3ebf0658812c1c2d0860c8751fff7dab46
Fauzan Valdera <f.valdera@yahoo.co.id>
2021-09-29 02:21:22 +0700

    Integrating a server-side rendering react app

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,21 @@
-const LandingPage = () => {
-  return <h1>LandingPage</h1>;
+import buildClient from "../api/build-client";
+
+const LandingPage = ({ currentUser }) => {
+  return currentUser ? (
+    <h1>You are signed in</h1>
+  ) : (
+    <h1>You are not signed in</h1>
+  );
+};
+
+LandingPage.getInitialProps = async (context) => {
+  const client = buildClient(context);
+  try {
+    const { data } = await client.get("/api/users/currentuser");
+    return data;
+  } catch (err) {
+    return { currentUser: null };
+  }
 };
 
 export default LandingPage;
